546603900fd0931402d3c419eeae1957b478fff1
Crypto Tachyon <cryptotachyondev@proton.me>
2024-04-20 10:51:01 +0200

    cleanup & init daisy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <button class="btn">Hello daisyui</button>
   </main>
 </template>
